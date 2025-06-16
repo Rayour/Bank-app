09b580b9fe5d435c1a8e988f7dc6de413704c4ca
Artemova Dina <artemova@perx.ru>
2025-06-16 19:00:36 +0300

    fix greeting func

--- src/views.py
+++ src/views.py
@@ -1,10 +1,10 @@
 import datetime
 
 
-def get_greeting() -> str:
+def get_greeting(hours: str) -> str:
     """Функция возвращает текст приветствия в зависимости от текущего времени"""
 
-    time_now = int(datetime.datetime.now().strftime('%H'))
+    time_now = int(hours)
     if 0 <= time_now < 6:
         greeting = "Доброй ночи"
     elif 6 <= time_now < 12:
@@ -15,5 +15,3 @@ def get_greeting() -> str:
         greeting = "Добрый вечер"
 
     return greeting
-
-
